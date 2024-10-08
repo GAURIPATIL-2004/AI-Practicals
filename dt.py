471ecdde70a3772ecf3825e9a4fe4d5ec0db1c35
GAURIPATIL <125655950+GAURIPATIL-2004@users.noreply.github.com>
2024-10-08 21:33:33 +0530

    Update dt.py make it more easy to understand

--- dt.py
+++ dt.py
@@ -1,38 +1,19 @@
-import pandas as pd  
-import numpy as np  
-import matplotlib.pyplot as plt  
-import seaborn as sns  
-from sklearn.datasets import load_iris  
-from sklearn.model_selection import train_test_split  
-from sklearn.tree import DecisionTreeClassifier, plot_tree  
-from sklearn import metrics  
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 
-# Load dataset and convert to DataFrame
-iris = load_iris()  
-data = pd.DataFrame(data=iris.data, columns=iris.feature_names)  
-data['Species'] = iris.target  
-data['Species'] = data['Species'].replace(dict(zip(np.unique(iris.target), np.unique(iris.target_names))))  
+# Load data
+iris = load_iris()
+X_train, X_test, Y_train, Y_test = train_test_split(iris.data, iris.target, test_size=0.01, random_state=10)
 
-# Split dataset into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(data.drop("Species", axis=1), data['Species'], test_size=0.3, random_state=93)
+# Train model
+clf = DecisionTreeClassifier().fit(X_train, Y_train)
 
-# Fit decision tree model
-dtc = DecisionTreeClassifier(max_depth=3, random_state=93)  
-dtc.fit(x_train, y_train)  
+# Print accuracy
+print(f'Accuracy: {clf.score(X_test, Y_test):.2f}')
 
-# Plot Decision Tree
-plt.figure(figsize=(20, 7))  
-plot_tree(dtc, feature_names=iris.feature_names, class_names=dtc.classes_, rounded=True, filled=True, fontsize=10)  
-plt.show()  
-
-# Predict and create confusion matrix
-y_pred = dtc.predict(x_test)  
-conf_matrix = metrics.confusion_matrix(y_test, y_pred)  
-
-# Plot heatmap of confusion matrix
-plt.figure(figsize=(10, 7))  
-sns.heatmap(pd.DataFrame(conf_matrix), annot=True, cmap="magma", fmt="g", xticklabels=dtc.classes_, yticklabels=dtc.classes_)  
-plt.title('Confusion Matrix')  
-plt.xlabel("Predicted Values")  
-plt.ylabel("True Labels")  
+# Plot tree
+plt.figure(figsize=(10, 6))
+plot_tree(clf, feature_names=iris.feature_names, class_names=iris.target_names, filled=True)
 plt.show()
